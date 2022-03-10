50cbc095f199a26bb724c5cfa3841df14f886c3d
acapodil <acapodilupo980@gmail.com>
2022-03-10 08:41:41 -0500

    Added script for ADConnect server

---
+++ Scripts/getADC2.ps1
@@ -0,0 +1,23 @@
+﻿
+    try{ 
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/acapodil/Azure-Virtual-Desktop/main/Scripts/install.ps1'))
+
+    }
+    catch{
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/acapodil/Azure-Virtual-Desktop/main/Scripts/install.ps1'))
+
+    }
+
+
+     try{ 
+            choco install azure-ad-connect -yes --ignore-checksums
+
+    }
+    catch{
+            choco install azure-ad-connect -yes --ignore-checksums
+
+    }
+
+
+
+    exit 0
